9cc3a8672f1819008a42d4d59cacd6575ad3b2cb
Ryan-Zayne <phoenixastro97@gmail.com>
2026-02-13 23:37:11 +0100

    updates

--- src/pages/(home)/-components/WhatsAppLink.tsx
+++ src/pages/(home)/-components/WhatsAppLink.tsx
@@ -1,9 +1,9 @@
 import type { InferProps } from "@zayne-labs/toolkit-react/utils";
 
-function WhatsAppLink(props: Omit<InferProps<"a">, "href" | "rel">) {
+function WhatsAppLink(props: Omit<InferProps<"a">, "href">) {
 	return (
 		<a
-			href="https://wa.me/2348145046180?text=Hello%2C+I+want+to+register+my+child+for+tech4teens"
+			href="https://wa.me/2348145046180?text=Hello%2C+I+want+to+register+my+child+for+the+Tech4teens+program"
 			target="_blank"
 			rel="noreferrer noopener"
 			{...props}
