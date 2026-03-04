d2a88e4d90d5b9f64893908e8a0fad76483762f0
Ryan-Zayne <phoenixastro97@gmail.com>
2026-03-04 13:37:56 +0100

    updates to link

--- src/pages/(home)/-components/WhatsAppLink.tsx
+++ src/pages/(home)/-components/WhatsAppLink.tsx
@@ -2,18 +2,10 @@ import type { InferProps } from "@zayne-labs/toolkit-react/utils";
 
 const WHATSAPP_MESSAGE = "Hello, I want to register my ward for the Tech4teens program";
 const WHATSAPP_PHONE = "2348145046180";
+const whatsappUrl = `https://wa.me/${WHATSAPP_PHONE}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
 
 function WhatsAppLink(props: Omit<InferProps<"a">, "href">) {
-	const whatsappUrl = `https://wa.me/${WHATSAPP_PHONE}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
-
-	return (
-		<a
-			href={whatsappUrl}
-			target="_blank"
-			rel="noreferrer noopener"
-			{...props}
-		/>
-	);
+	return <a href={whatsappUrl} target="_blank" rel="noreferrer noopener" {...props} />;
 }
 
 export { WhatsAppLink };
